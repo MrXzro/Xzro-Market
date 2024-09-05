fd6f9e02393d20d2aa7fd7b68b2b0db813e71f04
Xzro <admin@xzro.com>
2024-09-05 10:29:08 +0800

    增加分组删除功能，新增修改接口

--- admin-front/src/api/ggroupsApi.js
+++ admin-front/src/api/ggroupsApi.js
@@ -2,6 +2,12 @@ import service from ".";
 const ggroupsApi = {
     selectAll(){
         return service.get(`/api/ggroups/selectAll`);
+    },
+    delete(id){
+        return service.post("/api/ggroups/deleteById",{id})
+    },
+    insert(data){
+        return service.post("/api/ggroups/insertGgroup",data)
     }
 
 }
