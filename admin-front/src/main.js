016014c995e524eeb2cb240f57f44db276e70dfb
Xzro <admin@xzro.com>
2024-09-12 16:53:41 +0800

    使用Pinia持久化保存鉴权信息

--- admin-front/src/main.js
+++ admin-front/src/main.js
@@ -3,8 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import { createPinia } from 'pinia'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+import pinia from './stores'  //引入
 
 import App from './App.vue'
 import router from './router'
@@ -14,7 +15,7 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
   }
   
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(ElementPlus)
 app.mount('#app')
